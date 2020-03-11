ffd1acd316cb5f9d14df3c3a330919a2a3c742f2
Steven Barash <stevenbarash6@gmail.com>
2020-03-11 16:59:37 -0400

    commit

---
+++ build/precache-manifest.006eac36305710da9e36d13af007e942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913017973d63d96db0b149b42a4e6431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff0e5a492c058a2468ff",
+    "url": "/static/css/main.c49a72a3.chunk.css"
+  },
+  {
+    "revision": "61131e474493e50f26f3",
+    "url": "/static/js/2.eeacaced.chunk.js"
+  },
+  {
+    "revision": "5493ac8c9e221eeba3bec532634c6faf",
+    "url": "/static/js/2.eeacaced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0e5a492c058a2468ff",
+    "url": "/static/js/main.84d73f1f.chunk.js"
+  },
+  {
+    "revision": "a8eec2670e82d65631b3",
+    "url": "/static/js/runtime-main.c6d33bbb.js"
+  }
+]);
